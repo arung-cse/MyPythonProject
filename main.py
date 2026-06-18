e923f6082283f7266a1fcc5536aa456515b8aa68
arung-cse <arungcse2@gmail.com>
2026-06-18 20:08:34 +0530

    Added job selection system

--- main.py
+++ main.py
@@ -4,10 +4,31 @@ name = input("Enter Character Name: ")
 
 level = 1
 hp = 100
-job = "Novice"
 
-print("\nCharacter Created!")
-print("Name:", name)
-print("Job:", job)
+print("\nChoose Your Job")
+print("1. Warrior")
+print("2. Mage")
+print("3. Archer")
+
+choice = input("Enter Choice: ")
+
+if choice == "1":
+    job = "Warrior"
+    hp = 150
+
+elif choice == "2":
+    job = "Mage"
+    hp = 80
+
+elif choice == "3":
+    job = "Archer"
+    hp = 100
+
+else:
+    job = "Novice"
+
+print("\n=== Character Created ===")
+print("Name :", name)
+print("Job  :", job)
 print("Level:", level)
-print("HP:", hp)
+print("HP   :", hp)
