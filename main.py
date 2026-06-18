60a280c3ad040148be0599014b4784d42ffa193e
arung-cse <arungcse2@gmail.com>
2026-06-18 20:05:50 +0530

    Added character creation system

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello GitHub!")
+print("=== MMORPG Character Simulator ===")
+
+name = input("Enter Character Name: ")
+
+level = 1
+hp = 100
+job = "Novice"
+
+print("\nCharacter Created!")
+print("Name:", name)
+print("Job:", job)
+print("Level:", level)
+print("HP:", hp)
